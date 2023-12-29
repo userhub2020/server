e04a196454bc1fc1e238cfde4531f4ded68ab307
userhub2020 <73968050+userhub2020@users.noreply.github.com>
2023-12-29 23:51:45 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,7 +10,7 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 4; index++) {
+    for (let index = 0; index <= 3; index++) {
         radio.sendNumber(index + 101)
     }
     gamestate = 0
