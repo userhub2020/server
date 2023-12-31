7d8805ab5db29d2bf1f8085b8aa6b78dcfa5cb78
userhub2020 <73968050+userhub2020@users.noreply.github.com>
2023-12-31 10:52:55 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,20 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (gamestate == 0) {
-        if (0 < receivedNumber && receivedNumber < 50) {
-            if (list.indexOf(receivedNumber) < 0) {
-                list.unshift(receivedNumber)
-                led.plot((receivedNumber - 1) % 5, (receivedNumber - 1) / 5)
-                radio.sendNumber(receivedNumber + 50)
-            }
+    if (0 < receivedNumber && receivedNumber < 50) {
+        if (list.indexOf(receivedNumber) < 0) {
+            list.unshift(receivedNumber)
+            led.plot((receivedNumber - 1) % 5, (receivedNumber - 1) / 5)
+            radio.sendNumber(receivedNumber + 50)
         }
-        if (100 < receivedNumber && receivedNumber < 150) {
-            if (list.indexOf(receivedNumber - 100) >= 0) {
-                list.removeAt(list.indexOf(receivedNumber - 100))
-                led.unplot((receivedNumber - 101) % 5, (receivedNumber - 101) / 5)
-            }
+    }
+    if (100 < receivedNumber && receivedNumber < 150) {
+        if (list.indexOf(receivedNumber - 100) >= 0) {
+            list.removeAt(list.indexOf(receivedNumber - 100))
+            led.unplot((receivedNumber - 101) % 5, (receivedNumber - 101) / 5)
             radio.sendNumber(receivedNumber + 50)
         }
     }
 })
 input.onButtonPressed(Button.A, function () {
-    gamestate = 0
     list = []
     for (let index = 0; index <= 3; index++) {
         radio.sendNumber(index + 151)
@@ -31,21 +28,16 @@ input.onButtonPressed(Button.A, function () {
         `)
 })
 input.onButtonPressed(Button.B, function () {
-    if (gamestate == 0) {
-        indexPicked = randint(0, list.length - 1)
-        picked = list[indexPicked]
-        list.removeAt(indexPicked)
-        gamestate = 1
-        radio.sendNumber(200 + picked)
-    }
+    indexPicked = randint(0, list.length - 1)
+    picked = list[indexPicked]
+    list.removeAt(indexPicked)
+    radio.sendNumber(200 + picked)
 })
 let picked = 0
 let indexPicked = 0
-let gamestate = 0
 let list: number[] = []
 radio.setGroup(4)
 list = []
-gamestate = 0
 basic.showLeds(`
     . . . . .
     . . . . .
